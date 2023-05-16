1a2a3816e08665b05dcee6352a3f845d61749c61
Agajan Torayev <torayeff@gmail.com>
2023-05-17 00:01:07 +0100

    better code base but worse lb results

--- nbs/baseline.py
+++ nbs/baseline.py
@@ -3,25 +3,28 @@ from pathlib import Path
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
 
 data_path = Path("../data/")
+seed = 42
 
 
-def balanced_log_loss(y_true, pred_probs):
-    pred_probs = np.maximum(np.minimum(pred_probs, 1 - 1e-15), 1e-15)
-    pred_probs = pred_probs / np.sum(pred_probs, axis=1)[:, None]
+def balanced_log_loss(y_true, y_pred):
+    y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
 
-    n0, n1 = np.bincount(y_true)
+    n0, n1 = np.bincount(y_true.astype(int))
     w0 = 1 / (n0 / len(y_true))
     w1 = 1 / (n1 / len(y_true))
 
-    l0 = -w0 / n0 * np.sum(np.where(y_true == 0, 1, 0) * np.log(pred_probs[:, 0]))
-    l1 = -w1 / n1 * np.sum(np.where(y_true == 1, 1, 0) * np.log(pred_probs[:, 1]))
+    l0 = -w0 / n0 * np.sum(np.where(y_true == 0, 1, 0) * np.log(1 - y_pred))
+    l1 = -w1 / n1 * np.sum(np.where(y_true != 0, 1, 0) * np.log(y_pred))
 
     return (l0 + l1) / (w0 + w1)
+
+
+def lgb_metric(y_true, y_pred):
+    return "balanced_log_loss", balanced_log_loss(y_true, y_pred), False
 
 
 # read data
@@ -41,36 +44,34 @@ X_train, y_train = train_df[feature_columns], train_df["Class"]
 X_test = test_df[feature_columns]
 
 n_splits = 5
-kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
 test_probs = np.zeros((len(test_df), 2))
 
+cur_split = 1
 for train_idx, val_idx in kf.split(X_train, y_train):
+    print(f"Fold: {cur_split}".center(100, "-"))
+    cur_split += 1
+
     X_train_fold, y_train_fold = (X_train.iloc[train_idx], y_train.iloc[train_idx])
     X_val_fold, y_val_fold = X_train.iloc[val_idx], y_train.iloc[val_idx]
 
     params = {
         "objective": "binary",
-        "metric": "binary_logloss",
-        "verbosity": -1,
-        "random_state": 42,
+        "n_estimators": 10000,
+        "n_jobs": -1,
+        "verbose": -1,
+        "seed": seed,
     }
-
-    model = lgb.LGBMClassifier(n_estimators=1000, **params)
+    model = lgb.LGBMClassifier(**params)
     model.fit(
         X_train_fold,
         y_train_fold,
         eval_set=[(X_val_fold, y_val_fold)],
-        early_stopping_rounds=50,
-        verbose=False,
-    )
-
-    val_probs = model.predict_proba(X_val_fold)
-    val_pred = model.predict(X_val_fold)
-
-    acc = accuracy_score(y_val_fold, val_pred)
-    print(
-        f"Validation accuracy: {acc:.4f}.",
-        f"Balanced log loss: {balanced_log_loss(y_val_fold, val_probs):.4f}",
+        eval_metric=["binary_logloss", lgb_metric],
+        callbacks=[
+            lgb.log_evaluation(period=-1),
+            lgb.early_stopping(stopping_rounds=5, verbose=True),
+        ],
     )
 
     test_fold_probs = model.predict_proba(X_test)
